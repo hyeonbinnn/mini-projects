2fec18ade1caf690a64db5b7b8e3bf28bd7425c2
hyeonbin jeong <duafuuu@gmail.com>
2023-05-13 02:40:03 +0900

    rename

--- image-slide/slide.js
+++ image-slide/slide.js
@@ -1,13 +1,14 @@
 const loc = 0;
-document.querySelector(".btn1").addEventListener("click", function () {
+
+document.querySelector('.btn1').addEventListener('click', function () {
   if (loc < 0) {
     loc += 100;
-    document.querySelector(".slide").style.transform = `translate(${loc}vw)`;
+    document.querySelector('.slide').style.transform = `translate(${loc}vw)`;
   }
 });
-document.querySelector(".btn2").addEventListener("click", function () {
+document.querySelector('.btn2').addEventListener('click', function () {
   if (loc > -200) {
     loc -= 100;
   }
-  document.querySelector(".slide").style.transform = `translate(${loc}vw)`;
+  document.querySelector('.slide').style.transform = `translate(${loc}vw)`;
 });
